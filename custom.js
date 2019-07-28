3af17c143613ada3a0ec04654d781cc1fafcda99
biemIslam <bellomaryam@ymail.com>
2019-07-28 20:36:45 +0100

    new modifications

---
+++ custom.js
@@ -0,0 +1,7 @@
+
+function modalForm(){
+    let modal = document.getElementById('modal-login');
+    let btn = document.getElementById('btnLogin');
+    let close = document.getElementsByClassName('close')[0];
+
+}             
